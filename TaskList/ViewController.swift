0ddee0ee75252e9208b13054c0298fdccf1f7604
Shamkhan Mutuskhanov <inkomman@gmail.com>
2023-07-31 12:28:02 +0300

    Initial Commit

---
+++ TaskList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskList
+//
+//  Created by Shamkhan Mutuskhanov on 31.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
